f8d2846cef9de93a0312663d0bf4e8ec9a70683b
anatolij <anatolijzdanov4@gmail.com>
2023-06-29 21:03:51 +0300

    task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,31 +1,51 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
-System.Console.WriteLine("Введите количество элементов массива:  ");
-int tempArray = Convert.ToInt32(Console.ReadLine());
-double[] randomArray = new double[tempArray];
 
-void mas(int tempArray)
+
+
+void FillArrayRandomNumbers(double[] numbers)
 {
     Random rand = new Random();
-    for (int i = 0; i < tempArray; i++)
-    {
-        double number = Math.Round(rand.Next(0, 10) + rand.NextDouble(), 3);
-        System.Console.WriteLine(number);
-    }
-
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Math.Round(rand.Next(0, 10) + rand.NextDouble(), 3);
+        }
 }
-int min = 0;
-int max = 0;
-for(int i = 0; i < tempArray; i++)
+void PrintArray(double[] numbers)
 {
-    if (min > i) min = tempArray;
-    if (max < i) max = tempArray;
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
 }
- mas(tempArray);
-Console.WriteLine($"Максимальный элемент: {max} Минимальный элемент: {min}");
-Console.WriteLine($"Разница между максимальным и минимальным элементом: {max-min}");
+
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+}
+
+Console.WriteLine("массив вещественных чисел: ");
+Console.WriteLine($" Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
 
 
 
- 
